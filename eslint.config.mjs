05bd34607238b71c13932fd0065c5c1979b139c8
katerynakolodynska <kolodinkata@gmail.com>
2025-08-07 03:42:57 +0200

    edit esling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["**/*.tsx", "**/*.ts", "**/*.js"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
